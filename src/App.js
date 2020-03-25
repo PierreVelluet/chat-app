27cbc7cd3385e9186727d504cd8f4b579be64d30
PierreVelluet <velluetp@gmail.com>
2020-03-25 21:04:31 +0100

    job done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import Contact from './components/Contact'
 import PropTypes from 'prop-types';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        online
-        avatar='https://randomuser.me/api/portraits/men/87.jpg'
-        name='Hunter Johnston'
-      />
-      <Contact
-        avatar='https://randomuser.me/api/portraits/women/16.jpg'
-        name='Kitty Day'
-      />
-      <Contact
-        online
-        avatar='https://randomuser.me/api/portraits/women/23.jpg'
-        name='Brandy Chapman'
-      />
+      <ContactList />
     </div>
   );
 }
